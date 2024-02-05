8d18a3dceb275b26ce82e7780b0200986faf77fc
Mahmoud Ebrahimkhani <mahmoudebrahimkhani@1910-macbook-30.lan>
2024-02-05 16:22:27 -0500

    updated extract_pdbbind_pockets.py

--- utils/extract_pdbbind_pockets.py
+++ utils/extract_pdbbind_pockets.py
@@ -17,93 +17,99 @@ from torch_scatter import scatter
 KMAP = {'Ki': 1, 'Kd': 2, 'IC50': 3}
 
 
-def get_ligand_atom_features(rdmol):
-    num_atoms = rdmol.GetNumAtoms()
-    atomic_number = []
-    aromatic = []
-    hybrid = []
-    degree = []
-    for atom_idx in range(num_atoms):
-        atom = rdmol.GetAtomWithIdx(atom_idx)
-        atomic_number.append(atom.GetAtomicNum())
-        aromatic.append(1 if atom.GetIsAromatic() else 0)
-        hybridization = atom.GetHybridization()
-        HYBRID_TYPES = {t: i for i, t in enumerate(HybridizationType.names.values())}
-        hybrid.append(HYBRID_TYPES[hybridization])
-        degree.append(atom.GetDegree())
-    node_type = torch.tensor(atomic_number, dtype=torch.long)
+def parse_pdbbind_index_file(raw_path, subset='refined'):
+    all_index = []
+    version = int(raw_path.rstrip('/')[-4:])
+    assert version >= 2016
+    if subset == 'refined':
+        data_path = os.path.join(raw_path, f'refined-set')
+        index_path = os.path.join(data_path, 'index', f'INDEX_refined_data.{version}')
+    elif subset == 'general':
+        data_path = os.path.join(raw_path, f'general-set-except-refined')
+        index_path = os.path.join(data_path, 'index', f'INDEX_general_PL_data.{version}')
+    else:
+        raise ValueError(subset)
 
-    row, col = [], []
-    for bond in rdmol.GetBonds():
-        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-        row += [start, end]
-        col += [end, start]
-    row = torch.tensor(row, dtype=torch.long)
-    col = torch.tensor(col, dtype=torch.long)
-    hs = (node_type == 1).to(torch.float)
-    num_hs = scatter(hs[row], col, dim_size=num_atoms).numpy()
-    feat_mat = np.array([atomic_number, aromatic, degree, num_hs, hybrid], dtype=np.long).transpose()
-    return feat_mat
+    all_files = os.listdir(data_path)
+    with open(index_path, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith('#'): continue
+        index, res, year, pka, kv = line.split('//')[0].strip().split()
+        kind = [v for k, v in KMAP.items() if k in kv]
+        assert len(kind) == 1
+        if index in all_files:
+            all_index.append([index, res, year, pka, kind[0]])
+    return all_index
 
 
-def parse_sdf_file_mol(path, heavy_only=True, mol=None):
-    if mol is None:
-        if path.endswith('.sdf'):
-            mol = Chem.MolFromMolFile(path, sanitize=False)
-        elif path.endswith('.mol2'):
-            mol = Chem.MolFromMol2File(path, sanitize=False)
+def process_item(item, args):
+    pdb_idx, res, year, pka, kind = item
+    try:
+        if args.subset == 'refined':
+            pdb_path = os.path.join(args.source, 'refined-set', pdb_idx)
+        elif args.subset == 'general':
+            pdb_path = os.path.join(args.source, 'general-set-except-refined', pdb_idx)
         else:
-            raise ValueError
+            raise ValueError(args.subset)
+
+        protein_path = os.path.join(pdb_path, f'{pdb_idx}_protein.pdb')
+        ligand_sdf_path = os.path.join(pdb_path, f'{pdb_idx}_ligand.sdf')
+        ligand_mol2_path = os.path.join(pdb_path, f'{pdb_idx}_ligand.mol2')
+        mol, problem, ligand_path = read_mol(ligand_sdf_path, ligand_mol2_path)
+        if problem:
+            print('Read mol error.', item)
+            return None, ligand_path, res, pka, kind
+
+        protein = PDBProtein(protein_path)
+        ligand = parse_sdf_file_mol(ligand_path, heavy_only=False)
+        pocket_path = os.path.join(pdb_path, f'{pdb_idx}_pocket{args.radius}.pdb')
+        if not os.path.exists(pocket_path):
+            pdb_block_pocket = protein.residues_to_pdb_block(
+                protein.query_residues_ligand(ligand, args.radius)
+            )
+            with open(pocket_path, 'w') as f:
+                f.write(pdb_block_pocket)
+        return pocket_path, ligand_path, res, pka, kind
+
+    except Exception:
+        print('Exception occured.', item)
+        return None, ligand_path, res, pka, kind
+    
+
+def read_mol(sdf_fileName, mol2_fileName, verbose=False):
+    Chem.WrapLogs()
+    stderr = sys.stderr
+    sio = sys.stderr = StringIO()
+    mol = Chem.MolFromMolFile(sdf_fileName, sanitize=False)
+    problem = False
+    ligand_path = None
+    try:
         Chem.SanitizeMol(mol)
-        if heavy_only:
+        mol = Chem.RemoveHs(mol)
+        sm = Chem.MolToSmiles(mol)
+        ligand_path = sdf_fileName
+    except Exception as e:
+        sm = str(e)
+        problem = True
+    if problem:
+        mol = Chem.MolFromMol2File(mol2_fileName, sanitize=False)
+        problem = False
+        try:
+            Chem.SanitizeMol(mol)
             mol = Chem.RemoveHs(mol)
-            
-    feat_mat = get_ligand_atom_features(mol)
+            sm = Chem.MolToSmiles(mol)
+            problem = False
+            ligand_path = mol2_fileName
+        except Exception as e:
+            sm = str(e)
+            problem = True
 
-    ptable = Chem.GetPeriodicTable()
-
-    num_atoms = mol.GetNumAtoms()
-    pos = mol.GetConformer().GetPositions()
-
-    element = []
-    accum_pos = np.array([0.0, 0.0, 0.0], dtype=np.float32)
-    accum_mass = 0.0
-    for atom_idx in range(num_atoms):
-        atom = mol.GetAtomWithIdx(atom_idx)
-        atomic_number = atom.GetAtomicNum()
-        element.append(atomic_number)
-        x, y, z = pos[atom_idx]
-        atomic_weight = ptable.GetAtomicWeight(atomic_number)
-        accum_pos += np.array([x, y, z]) * atomic_weight
-        accum_mass += atomic_weight
-    center_of_mass = np.array(accum_pos / accum_mass, dtype=np.float32)
-    element = np.array(element, dtype=np.int)
-    pos = np.array(pos, dtype=np.float32)
-
-    row, col, edge_type = [], [], []
-    BOND_TYPES = {t: i for i, t in enumerate(BondType.names.values())}
-    for bond in mol.GetBonds():
-        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-        row += [start, end]
-        col += [end, start]
-        edge_type += 2 * [BOND_TYPES[bond.GetBondType()]]
-    edge_index = np.array([row, col], dtype=np.long)
-    edge_type = np.array(edge_type, dtype=np.long)
-    perm = (edge_index[0] * num_atoms + edge_index[1]).argsort()
-    edge_index = edge_index[:, perm]
-    edge_type = edge_type[perm]
-
-    data = {
-        'element': element,
-        'pos': pos,
-        'bond_index': edge_index,
-        'bond_type': edge_type,
-        'center_of_mass': center_of_mass,
-        'atom_feature': feat_mat
-    }
-    return data
-
-
+    if verbose:
+        print(sio.getvalue())
+    sys.stderr = stderr
+    return mol, problem, ligand_path
+    
 class PDBProtein(object):
     AA_NAME_SYM = {
         'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E', 'PHE': 'F', 'GLY': 'G', 'HIS': 'H',
@@ -282,105 +288,96 @@ class PDBProtein(object):
         return block
 
 
-def read_mol(sdf_fileName, mol2_fileName, verbose=False):
-    Chem.WrapLogs()
-    stderr = sys.stderr
-    sio = sys.stderr = StringIO()
-    mol = Chem.MolFromMolFile(sdf_fileName, sanitize=False)
-    problem = False
-    ligand_path = None
-    try:
-        Chem.SanitizeMol(mol)
-        mol = Chem.RemoveHs(mol)
-        sm = Chem.MolToSmiles(mol)
-        ligand_path = sdf_fileName
-    except Exception as e:
-        sm = str(e)
-        problem = True
-    if problem:
-        mol = Chem.MolFromMol2File(mol2_fileName, sanitize=False)
-        problem = False
-        try:
-            Chem.SanitizeMol(mol)
-            mol = Chem.RemoveHs(mol)
-            sm = Chem.MolToSmiles(mol)
-            problem = False
-            ligand_path = mol2_fileName
-        except Exception as e:
-            sm = str(e)
-            problem = True
-
-    if verbose:
-        print(sio.getvalue())
-    sys.stderr = stderr
-    return mol, problem, ligand_path
-
-
-def parse_pdbbind_index_file(raw_path, subset='refined'):
-    all_index = []
-    version = int(raw_path.rstrip('/')[-4:])
-    assert version >= 2016
-    if subset == 'refined':
-        data_path = os.path.join(raw_path, f'refined-set')
-        index_path = os.path.join(data_path, 'index', f'INDEX_refined_data.{version}')
-    elif subset == 'general':
-        data_path = os.path.join(raw_path, f'general-set-except-refined')
-        index_path = os.path.join(data_path, 'index', f'INDEX_general_PL_data.{version}')
-    else:
-        raise ValueError(subset)
-
-    all_files = os.listdir(data_path)
-    with open(index_path, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        if line.startswith('#'): continue
-        index, res, year, pka, kv = line.split('//')[0].strip().split()
-        kind = [v for k, v in KMAP.items() if k in kv]
-        assert len(kind) == 1
-        if index in all_files:
-            all_index.append([index, res, year, pka, kind[0]])
-    return all_index
-
-
-def process_item(item, args):
-    pdb_idx, res, year, pka, kind = item
-    try:
-        if args.subset == 'refined':
-            pdb_path = os.path.join(args.source, 'refined-set', pdb_idx)
-        elif args.subset == 'general':
-            pdb_path = os.path.join(args.source, 'general-set-except-refined', pdb_idx)
+def parse_sdf_file_mol(path, heavy_only=True, mol=None):
+    if mol is None:
+        if path.endswith('.sdf'):
+            mol = Chem.MolFromMolFile(path, sanitize=False)
+        elif path.endswith('.mol2'):
+            mol = Chem.MolFromMol2File(path, sanitize=False)
         else:
-            raise ValueError(args.subset)
+            raise ValueError
+        Chem.SanitizeMol(mol)
+        if heavy_only:
+            mol = Chem.RemoveHs(mol)
+            
+    feat_mat = get_ligand_atom_features(mol)
 
-        protein_path = os.path.join(pdb_path, f'{pdb_idx}_protein.pdb')
-        ligand_sdf_path = os.path.join(pdb_path, f'{pdb_idx}_ligand.sdf')
-        ligand_mol2_path = os.path.join(pdb_path, f'{pdb_idx}_ligand.mol2')
-        mol, problem, ligand_path = read_mol(ligand_sdf_path, ligand_mol2_path)
-        if problem:
-            print('Read mol error.', item)
-            return None, ligand_path, res, pka, kind
+    ptable = Chem.GetPeriodicTable()
 
-        protein = PDBProtein(protein_path)
-        ligand = parse_sdf_file_mol(ligand_path, heavy_only=False)
-        pocket_path = os.path.join(pdb_path, f'{pdb_idx}_pocket{args.radius}.pdb')
-        if not os.path.exists(pocket_path):
-            pdb_block_pocket = protein.residues_to_pdb_block(
-                protein.query_residues_ligand(ligand, args.radius)
-            )
-            with open(pocket_path, 'w') as f:
-                f.write(pdb_block_pocket)
-        return pocket_path, ligand_path, res, pka, kind
+    num_atoms = mol.GetNumAtoms()
+    pos = mol.GetConformer().GetPositions()
 
-    except Exception:
-        print('Exception occured.', item)
-        return None, ligand_path, res, pka, kind
+    element = []
+    accum_pos = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+    accum_mass = 0.0
+    for atom_idx in range(num_atoms):
+        atom = mol.GetAtomWithIdx(atom_idx)
+        atomic_number = atom.GetAtomicNum()
+        element.append(atomic_number)
+        x, y, z = pos[atom_idx]
+        atomic_weight = ptable.GetAtomicWeight(atomic_number)
+        accum_pos += np.array([x, y, z]) * atomic_weight
+        accum_mass += atomic_weight
+    center_of_mass = np.array(accum_pos / accum_mass, dtype=np.float32)
+    element = np.array(element, dtype=np.int)
+    pos = np.array(pos, dtype=np.float32)
 
+    row, col, edge_type = [], [], []
+    BOND_TYPES = {t: i for i, t in enumerate(BondType.names.values())}
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        row += [start, end]
+        col += [end, start]
+        edge_type += 2 * [BOND_TYPES[bond.GetBondType()]]
+    edge_index = np.array([row, col], dtype=np.long)
+    edge_type = np.array(edge_type, dtype=np.long)
+    perm = (edge_index[0] * num_atoms + edge_index[1]).argsort()
+    edge_index = edge_index[:, perm]
+    edge_type = edge_type[perm]
+
+    data = {
+        'element': element,
+        'pos': pos,
+        'bond_index': edge_index,
+        'bond_type': edge_type,
+        'center_of_mass': center_of_mass,
+        'atom_feature': feat_mat
+    }
+    return data
+
+
+def get_ligand_atom_features(rdmol):
+    num_atoms = rdmol.GetNumAtoms()
+    atomic_number = []
+    aromatic = []
+    hybrid = []
+    degree = []
+    for atom_idx in range(num_atoms):
+        atom = rdmol.GetAtomWithIdx(atom_idx)
+        atomic_number.append(atom.GetAtomicNum())
+        aromatic.append(1 if atom.GetIsAromatic() else 0)
+        hybridization = atom.GetHybridization()
+        HYBRID_TYPES = {t: i for i, t in enumerate(HybridizationType.names.values())}
+        hybrid.append(HYBRID_TYPES[hybridization])
+        degree.append(atom.GetDegree())
+    node_type = torch.tensor(atomic_number, dtype=torch.long)
+
+    row, col = [], []
+    for bond in rdmol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        row += [start, end]
+        col += [end, start]
+    row = torch.tensor(row, dtype=torch.long)
+    col = torch.tensor(col, dtype=torch.long)
+    hs = (node_type == 1).to(torch.float)
+    num_hs = scatter(hs[row], col, dim_size=num_atoms).numpy()
+    feat_mat = np.array([atomic_number, aromatic, degree, num_hs, hybrid], dtype=np.long).transpose()
+    return feat_mat
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default='ml_data/mahmoud/pdbbind/pdbbind_v2020')
-    parser.add_argument('--fixed_sdf_directory', type=str, default='ml_data/mahmoud/pdbbind_v2020/fixed_sdf_files')
     parser.add_argument('--subset', type=str, default='refined')
     parser.add_argument('--refined_index_pkl', type=str, default=None)
     parser.add_argument('--radius', type=int, default=10)
